ef7018036844f46d11d3e2f918ce44553cad3689
Oddant1 <oddant1@hotmail.com>
2024-03-19 15:35:25 -0700

    Streamline and comment

--- src/lib/components/Iframe.svelte
+++ src/lib/components/Iframe.svelte
@@ -1,16 +1,13 @@
 <script lang="ts">
   import readerModel from "$lib/models/readerModel";
 
-  // Taken from https://stackoverflow.com/a/8079681
+  // Adapted from https://stackoverflow.com/a/8079681
   //
-  // Creates an invisible div then calculates the difference between the width
-  // of that div with a scrollbar and without a scrollbar to get the scrollbar
-  // width
+  // Creates an invisible div with a 100% width child that overflows in y then
+  // calculates the difference between the width of the outer div and the width
+  // of the inner div with the scroll.
   function getScrollBarWidth () {
-    const inner = document.createElement('p');
-    inner.style.width = "100%";
-    inner.style.height = "200px";
-
+    // Create outer div
     const outer = document.createElement('div');
     outer.style.position = "absolute";
     outer.style.top = "0px";
@@ -18,23 +15,33 @@
     outer.style.visibility = "hidden";
     outer.style.width = "200px";
     outer.style.height = "150px";
-    outer.style.overflow = "hidden";
+    outer.style.overflow = "scroll";
+
+    // Create inner div that is taller than inner div
+    const inner = document.createElement('p');
+    inner.style.width = "100%";
+    inner.style.height = "200px";
+
+    // Make inner div a child of outer div
     outer.appendChild(inner);
-
+    // Add outer div to the document
     document.body.appendChild(outer);
-    const w1 = inner.offsetWidth;
-    outer.style.overflow = 'scroll';
-    let w2 = inner.offsetWidth;
-    if (w1 == w2) {
-      w2 = outer.clientWidth;
-    }
 
+    // Get the width of the inner div. Since this div is set to 100% width it
+    // will be the width of the outer div MINUS the width of the scrollbar
+    // because the scrollbar width cuts into the div width
+    const withScrollWidth = inner.offsetWidth;
+    // Get the width of the outer div which is the same as the width of the
+    // inner div would be without a scroll bar since the inner div width is set
+    // to 100% of the outer div width
+    const withoutScrollWidth = outer.offsetWidth;
+
+    // Remove this dummy div from the DOM
     document.body.removeChild(outer);
 
-    return (w1 - w2);
+    // Return difference in widths, this is the width of the scrollbar
+    return (withoutScrollWidth - withScrollWidth);
   };
-
-  console.log(getScrollBarWidth());
 </script>
 
 <!-- The width and left are calculated to fill the scroll-gutters on the
