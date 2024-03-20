1b39c7965edd1ba510133e9a02109ffda44ba65f
Oddant1 <oddant1@hotmail.com>
2024-03-20 09:59:57 -0700

    Add download link for bib to details

--- src/lib/components/Details.svelte
+++ src/lib/components/Details.svelte
@@ -1,15 +1,23 @@
 <script lang="ts">
-  import JSONTree from "svelte-json-tree";
-
   import Panel from "$lib/components/Panel.svelte";
   import readerModel from "$lib/models/readerModel";
   import ResultDetails from "$lib/components/ResultDetails.svelte";
+
+  const blob = new Blob([$readerModel.citations], { type: "text/plain" })
+  const download = URL.createObjectURL(blob);
 </script>
 
 <Panel header="Details of {$readerModel.name}">
   <ResultDetails name={$readerModel.name} resultJSON={$readerModel.metadata}/>
 </Panel>
 <Panel header="Citations">
+  <label for="citation-style">
+    Citation Format:
+    <select id="citation-style">
+      <option value="bibtex">BibTex</option>
+    </select>
+  </label>
+  <a href={download} download={`${$readerModel.metadata.uuid}.bib`} style="float: right">Download</a>
   <pre id="json">{$readerModel.citations}</pre>
 </Panel>
 
