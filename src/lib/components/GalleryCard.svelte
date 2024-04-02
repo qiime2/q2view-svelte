356a74f66b3187f0ace32f48e254630948e6b2bd
Oddant1 <oddant1@hotmail.com>
2024-04-02 11:29:09 -0700

    Add some padding under the title on the gallery cards and under the thumbnail

--- src/lib/components/GalleryCard.svelte
+++ src/lib/components/GalleryCard.svelte
@@ -15,8 +15,8 @@
 </script>
 
 <div class="gallery-card">
-  <h3 style="font-size: 24px;">{title}</h3>
-  <img src={img} alt={title}>
+  <h3 style="font-size: 24px;" class="pb-1">{title}</h3>
+  <img src={img} alt={title} class="pb-1">
   <p style="font-size: 14px">{desc}</p>
   <p class="galler-button-wrapper" style="font-size: 14px">
     <button class="gallery-button" on:click={galleryButton}>
