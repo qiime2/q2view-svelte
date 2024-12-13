3f6c02b4f7da8eb4c7572201b5171dc3695eadb2
Oddant1 <oddant1@hotmail.com>
2024-12-13 15:21:50 -0700

    whitespace

--- src/routes/(main)/+layout.svelte
+++ src/routes/(main)/+layout.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import { env } from "$env/dynamic/public";
-    import Main from "$lib/components/Main.svelte";
-    import Vendored from "$lib/components/Vendored.svelte";
+  import Main from "$lib/components/Main.svelte";
+  import Vendored from "$lib/components/Vendored.svelte";
 </script>
 
 {#if env.PUBLIC_VENDORED}
