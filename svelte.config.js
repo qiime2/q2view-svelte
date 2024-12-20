7501c7200e34b9a6b18fc565da2501bb4cb2a808
Oddant1 <oddant1@hotmail.com>
2024-12-20 12:41:05 -0700

    Add seperate build command with seperate route and no sw

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,12 @@
-import adapter from "@sveltejs/adapter-static";
-import { vitePreprocess } from "@sveltejs/kit/vite";
-import { preprocessMeltUI, sequence } from "@melt-ui/pp";
+import sw_config from "./sw-config.js"
+import vend_config from "./vend-config.js"
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
-  // for more information about preprocessors
-  preprocess: sequence([vitePreprocess(), preprocessMeltUI()]),
+let config;
 
-  kit: {
-    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
-    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-    adapter: adapter({ fallback: "index.html" }),
-  },
-};
+if (process.env.VENDORED) {
+  config = vend_config;
+} else {
+  config = sw_config;
+}
 
 export default config;
